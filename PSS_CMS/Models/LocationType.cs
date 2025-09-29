d2f8525ca788f46e6b1edb26850007e0297919de
Aakashpraveen <praveenaakash4@gmail.com>
2025-09-29 18:32:57 +0530

    commited by aakash

---
+++ PSS_CMS/Models/LocationType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PSS_CMS.Models
+{
+    public class LocationType
+    {
+        public int LT_RECID { get; set; }
+        public string LT_NAME { get; set; }
+        public int? LT_SORTORDER { get; set; }
+        public string LT_DISABLE { get; set; }
+        public int LT_CRECID { get; set; }
+    }
+}

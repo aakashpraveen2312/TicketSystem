91dc02d07d8d7adeded97e0a031f3428f0f80d96
yoge01 <yogeshbose2016@gmail.com>
2026-04-21 16:54:40 +0530

    Changes done by Yogesh

---
+++ PSS_CMS/Models/InvoicePayment.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace PSS_CMS.Models
+{
+    public class InvoicePayment
+    {
+        public int SerialNumber { get; set; }
+        public int PP_RECID { get; set; }
+        public int PP_SIHRECID { get; set; }
+        public int PP_CRECID { get; set; }
+        [DisplayName("Invoice Number")]
+        public string PP_INVOICENUMBER { get; set; }
+        [DisplayName("Mode of Payment")]
+        public string PP_MODEOFPAYMENT { get; set; }
+        [DisplayName("Date of Payment")]
+        public string PP_DATEOFPAYMENT { get; set; }
+        [DisplayName("Invoice Date")]
+        public string PP_INVOICEDATE { get; set; }
+        [DisplayName("Total Amount")]
+        public decimal PP_TOTALAMOUNT { get; set; }
+        [DisplayName("Paid Amount")]
+        public decimal PP_PAIDAMOUNT { get; set; }
+        [DisplayName("Balance Amount")]
+        public decimal PP_BALANCEAMOUNT { get; set; }
+        [DisplayName("Payment Status")]
+        public decimal PP_PAYMENTSTATUS { get; set; }
+    }
+
+    public class InvoicePaymentRootObjects
+    {
+        public string Message { get; set; }
+        public string Status { get; set; }
+        public List<InvoicePayment> Data { get; set; }
+    }
+    public class InvoicePaymentRootObject
+    {
+        public string Message { get; set; }
+        public string Status { get; set; }
+        public InvoicePayment Data { get; set; }
+    }
+}
